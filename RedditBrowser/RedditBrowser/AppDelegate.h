e0ad428d6eb3aa232cd04922e6dd360e0c010843
Alex <alex@Alexs-Mac-Pro.local>
2014-02-06 01:11:50 +0100

    first commit on RedditBrowser

---
+++ RedditBrowser/RedditBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RedditBrowser
+//
+//  Created by Alex on 06/02/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RedditBrowser/RedditBrowser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RedditBrowser
+//
+//  Created by Alex on 06/02/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
